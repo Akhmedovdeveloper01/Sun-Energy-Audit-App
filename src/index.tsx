eff5140d4f2b6b1fbba1908092d53c56b527efc7
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2026-04-03 16:25:35 +0500

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-// Include Telegram UI styles first
 import '@telegram-apps/telegram-ui/dist/styles.css';
 
 import ReactDOM from 'react-dom/client';
@@ -6,34 +5,37 @@ import { StrictMode } from 'react';
 import { retrieveLaunchParams } from '@tma.js/sdk-react';
 
 import { init } from './init.ts';
-
 import { Root } from './Root.tsx';
-
 import './index.css';
-
-
 import { EnvUnsupported } from './components/EnvUnsupported.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-try {
-  const launchParams = retrieveLaunchParams();
-  const { tgWebAppPlatform: platform } = launchParams;
-  const debug = (launchParams.tgWebAppStartParam || '').includes('debug')
-    || import.meta.env.DEV;
+// 👇 SHU YERGA YOZASAN
+async function bootstrap() {
+  try {
+    const launchParams = retrieveLaunchParams();
+    const { tgWebAppPlatform: platform } = launchParams;
 
-  await init({
-    debug,
-    eruda: debug && ['ios', 'android'].includes(platform),
-    mockForMacOS: platform === 'macos',
-  })
-    .then(() => {
-      root.render(
-        <StrictMode>
-          <Root />
-        </StrictMode>,
-      );
+    const debug =
+      (launchParams.tgWebAppStartParam || '').includes('debug') ||
+      import.meta.env.DEV;
+
+    await init({
+      debug,
+      eruda: debug && ['ios', 'android'].includes(platform),
+      mockForMacOS: platform === 'macos',
     });
-} catch (e) {
-  root.render(<EnvUnsupported />);
+
+    root.render(
+      <StrictMode>
+        <Root />
+      </StrictMode>
+    );
+  } catch (e) {
+    root.render(<EnvUnsupported />);
+  }
 }
+
+// 👇 FUNCTIONNI CHAQRISH
+bootstrap();
